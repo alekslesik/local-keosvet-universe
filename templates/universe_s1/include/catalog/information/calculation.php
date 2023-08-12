2ce14666f8a5e8f51452a8dea3894e51995e2b8d
alekslesik <alekslesik@gmail.com>
2023-08-12 18:06:21 +0300

    del

---
+++ templates/universe_s1/include/catalog/information/calculation.php
@@ -0,0 +1,10 @@
+<div class="catalog-information">
+    <div class="catalog-information-items">
+        <div class="catalog-information-item">
+            <div class="catalog-information-item-title">
+                Светотехнический расчет
+            </div>
+            <div class="catalog-information-item-description">
+            При заказе нашей продукции, наши квалифицированные сотрудники помогут Вам определиться с выбором светильников и сделают расчет освещенности абсолютно бесплатно. 
+Вы сможете увидеть визуализацию объекта, до приобретения и монтажа светодиодного оборудования.</div>
+</div>
